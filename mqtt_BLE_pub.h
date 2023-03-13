93a07cc3684bff3d9df989b529388293c91e79a1
GuerinSamuel <77736093+GuerinSamuel@users.noreply.github.com>
2023-03-13 16:02:12 +0100

    Create mqtt_BLE_pub.h

---
+++ mqtt_BLE_pub.h
@@ -0,0 +1,84 @@
+#include <WiFi.h>
+#include <PubSubClient.h>
+#include <BLEDevice.h>
+#include <BLEUtils.h>
+#include <BLEServer.h>
+
+#define SERVICE_UUID        "4fafc201-1fb5-459e-8fcc-c5c9c331914b"
+#define CHARACTERISTIC_UUID "beb5483e-36e1-4688-b7f5-ea07361b26a8"
+
+const char *ssid="Sam";
+const char *password="mbpn6568";
+
+const char *mqtt_server="test.mosquitto.org";
+const char *mqtt_client_id="esp32mqtt-zen";
+const char *mqtt_topic="srt/lesbv";
+const int  mqtt_reconnect_ms_delay=5000;
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+
+void setup() {
+  Serial.begin(115200);
+  setup_wifi();
+  client.setServer(mqtt_server, 1883);
+  setup_ble();
+}
+
+void setup_wifi(){
+  delay(10);
+  WiFi.begin(ssid,password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.println("[WiFi] awaiting connection...");
+  }
+  Serial.println("[WiFi] connected !");
+}
+
+void setup_ble(){
+  Serial.println("Starting BLE work!");
+
+  BLEDevice::init("Long name works now");
+  BLEServer *pServer = BLEDevice::createServer();
+  BLEService *pService = pServer->createService(SERVICE_UUID);
+  BLECharacteristic *pCharacteristic = pService->createCharacteristic(
+                                         CHARACTERISTIC_UUID,
+                                         BLECharacteristic::PROPERTY_READ |
+                                         BLECharacteristic::PROPERTY_WRITE
+                                       );
+
+  pCharacteristic->setValue("Hello World says Neil");
+  pService->start();
+  // BLEAdvertising *pAdvertising = pServer->getAdvertising();  // this still is working for backward compatibility
+  BLEAdvertising *pAdvertising = BLEDevice::getAdvertising();
+  pAdvertising->addServiceUUID(SERVICE_UUID);
+  pAdvertising->setScanResponse(true);
+  pAdvertising->setMinPreferred(0x06);  // functions that help with iPhone connections issue
+  pAdvertising->setMinPreferred(0x12);
+  BLEDevice::startAdvertising();
+  Serial.println("Characteristic defined! Now you can read it in your phone!");
+}
+
+void mqtt_reconnect() {
+  while (!client.connected()) {
+    Serial.println("[MQTT] trying to open connection to server...");
+    if (client.connect(mqtt_client_id)){
+      Serial.println("[MQTT] connected to server !");
+      // send message upon connection
+      client.publish(mqtt_topic,"{\"freq\": 869000000, \"syncword\": 126, \"spreadingFactor\": 8, \"signalBandwidth\": 31250}");
+    } else {
+      Serial.print("[MQTT] failed to connect to server, rc=");
+      Serial.print(client.state());
+      Serial.println(" trying again...");
+      delay(mqtt_reconnect_ms_delay);
+    }
+  }
+}
+
+void loop() {
+  if(!client.connected()){
+    mqtt_reconnect();
+  }
+  client.loop();
+  delay(2000);
+}
